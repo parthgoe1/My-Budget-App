0a25876a738d11b8a1bba38418cef23e0ed93679
Parth Goel <parth.179301138@muj.manipal.edu>
2019-01-17 02:43:17 +0530

    List View added

--- lib/screens/TopicList.dart
+++ lib/screens/TopicList.dart
@@ -1,35 +1,60 @@
 import 'package:flutter/material.dart';
 
 class TopicList extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
-
     return TopicListState();
   }
 }
 
 class TopicListState extends State<TopicList> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text('Budgets'),
       ),
-
+      body: getNoteListView(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('FAB clicked');
         },
-
         tooltip: 'Add Budget',
-
         child: Icon(Icons.add),
-
       ),
     );
-
   }
 
+  ListView getNoteListView() {
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.tealAccent,
+              child: Icon(Icons.lock_open),
+            ),
+
+            title: Text('January', style: titleStyle,),
+
+            subtitle: Text('Rs 2000'),
+            
+            trailing: Icon(Icons.delete, color: Colors.grey,),
+
+            onTap: () {
+              debugPrint('ListTile tapped');
+            },
+
+          ),
+        );
+      },
+    );
+  }
 }
